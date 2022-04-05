4cf20baa9daccf72af049eb2572f82406f2df7e0
jackson <jacksonphilip1874511@gmail.com>
2022-04-05 18:37:30 +0530

    JS no is +ve or -ve

---
+++ script.js
@@ -0,0 +1,10 @@
+var Num=parseInt(prompt('Enter a Number'))
+
+if(Num>0)
+console.log('IS a positive number');
+
+else if (num==0)
+console.log('IS a Neutral');
+
+else if (num<0)
+console.log("IS a Negative number");
